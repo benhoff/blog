169f0e3887c02218211dbe81b38096a235ede97e
Ben Hoff <hoff.benjamin.k@gmail.com>
2017-06-05 22:36:05 -0600

    added in theme for modifying and disqus comments

--- pelicanconf.py
+++ pelicanconf.py
@@ -18,14 +18,9 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
+THEME = 'themes/base_theme'
 
 """
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
-
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),)
--- publishconf.py
+++ publishconf.py
@@ -20,5 +20,5 @@ DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 
-#DISQUS_SITENAME = ""
+DISQUS_SITENAME = "https://bens-blog-1.disqus.com"
 #GOOGLE_ANALYTICS = ""

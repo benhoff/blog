f77304292277b1a4593469c13cb397f8f23102e7
Ben Hoff <hoff.benjamin.k@gmail.com>
2017-06-06 15:25:55 -0600

    added in a setup.py file, mostly to track dependcies

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="blog",
+    version=verstr,
+    description='blog',
+    url='https://github.com/benhoff/vexbot',
+    license='GPL3',
+    author='Ben Hoff',
+    author_email='beohoff@gmail.com',
+    packages= find_packages(), # exclude=['docs', 'tests']
+    install_requires=[
+        'pelican',
+        ],
+
+    extras_require={
+        'dev': ['flake8', 'twine'],
+    }
+)

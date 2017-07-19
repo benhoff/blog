07723dcca1f0d7035ea3b731f2a9574614a6c582
Ben Hoff <hoff.benjamin.k@gmail.com>
2017-07-18 20:59:32 -0600

    tweaks to facerecog code

---
+++ dist/facerecog/facerecog/__init__.py
@@ -0,0 +1,4 @@
+from .util import get_haarcascade_filepath
+from .face_detection import RecordVideo, FaceDetectionWidget, MainWidget
+
+__all__ = ['RecordVideo', 'FaceDetectionWidget', 'MainWidget', 'get_haarcascade_filepath']

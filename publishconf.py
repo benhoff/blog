105a105fbfbfc566564816372398ba9f38aba4f6
Ben Hoff <hoff.benjamin.k@gmail.com>
2019-11-22 04:37:52 -0500

    https/ads

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://benhoff.net'
+SITEURL = 'https://benhoff.net'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
